4d1e9816400a43fb88a95d2417be049482e08a11
grzesioo66 <grzesioo66@gmail.com>
2023-10-20 23:44:21 +0200

    Added viewport

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,26 @@ int main()
 	std::ofstream myfile;
   	myfile.open ("graphic_hw.ppm");
 
-	int nx = 800;
-	int ny = 600;
 
-	myfile << "P3\n" << nx << " " << ny << "\n255\n";
+	// We define aspect ratio of the screen, as well as size
+  	auto aspect_ratio = 16.0 / 9.0;
 
-	for(int j = ny-1; j >= 0; j--)
+	int image_width 	= 800;
+	int image_height 	= static_cast<int>(image_width / aspect_ratio);
+	image_height 		= (image_height < 1) ? 1 : image_height;
+
+	//We define the viewport size
+	auto viewport_height = 2.0;
+	auto viewport_width  = viewport_height * (static_cast<double>(image_width/image_height));
+
+
+	myfile << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+	for(int j = image_height-1; j >= 0; j--)
 	{
-		for(int i = 0; i < nx ; i++)
+		for(int i = 0; i < image_width ; i++)
 		{
-			auto pixel_color = color(double(i)/(nx-1), double(j)/(ny-1), 0);
+			auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
 
             write_color(myfile, pixel_color);
             
