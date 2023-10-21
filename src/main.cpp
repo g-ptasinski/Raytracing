a781f5d42070a46feba6c494b1b2534b59f7de67
grzesioo66 <grzesioo66@gmail.com>
2023-10-21 14:16:01 +0200

    Blend

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,55 @@
 #include "../include/color.h"
 #include "../include/ray.h"
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
 	std::ofstream myfile;
   	myfile.open ("graphic_hw.ppm");
 
-
 	// We define aspect ratio of the screen, as well as size
-  	auto aspect_ratio = 16.0 / 9.0;
+  	auto aspect_ratio 	= 16.0 / 9.0;
 
 	int image_width 	= 800;
 	int image_height 	= static_cast<int>(image_width / aspect_ratio);
 	image_height 		= (image_height < 1) ? 1 : image_height;
 
-	//We define the viewport size
-	auto viewport_height = 2.0;
-	auto viewport_width  = viewport_height * (static_cast<double>(image_width/image_height));
+	//We define camera parameters and the viewport size
+	auto focal_length 	= 1.0;
+    auto viewport_height= 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    auto camera_center 	= point3(0, 0, 0);
 
+    // Calculate vectors across horziontal and vertical viewport edges
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
 
+    // Calculate delta vectors pixel to pixel
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    //Location of upper left pixel
+    auto viewport_upper_left 	= camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc 			= viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);    
+
+    //Render itself
 	myfile << "P3\n" << image_width << " " << image_height << "\n255\n";
 
 	for(int j = image_height-1; j >= 0; j--)
 	{
 		for(int i = 0; i < image_width ; i++)
 		{
-			auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+			auto pixel_center 	= pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
+			auto ray_direction 	= pixel_center - camera_center; 
+			ray r(camera_center, ray_direction);
 
-            write_color(myfile, pixel_color);
-            
+			auto pixel_color = ray_color(r);
+            write_color(myfile, pixel_color);        
 		}
 	}
 	
